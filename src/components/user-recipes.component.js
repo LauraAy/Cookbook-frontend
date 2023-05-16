e4fcb89e4be3eebbbf4b79186692da82a6477d1f
laura_ay <laura.ay1564@gmail.com>
2023-05-15 17:38:19 -0700

    userRecipes page functional

--- src/components/user-recipes.component.js
+++ src/components/user-recipes.component.js
@@ -3,6 +3,7 @@ import recipeDataService from "../services/recipe.service";
 import { Link } from "react-router-dom";
 import AuthService from "../services/auth.service";
 import UserService from "../services/user.service.js"
+
 export default class RecipesList extends Component {
   constructor(props) {
     super(props);
@@ -40,7 +41,7 @@ export default class RecipesList extends Component {
     UserService.getUserRecipes(id)
       .then(response => {
         this.setState({
-          recipes: response.data
+          userRecipes: response.data
         });
         console.log(response.data);
       })
@@ -94,13 +95,126 @@ export default class RecipesList extends Component {
       });
   }
 
-  render() {
-    const { searchTitle, recipes, currentrecipe, currentIndex } = this.state;
 
+  render() {
+    const { searchTitle, userRecipes, currentrecipe, currentIndex } = this.state;
+  
+ 
     return (
+    <>
+
       <div>
-        <h1>Ciao</h1>
+        <h1>{userRecipes.username}</h1>
       </div>
-    );
-  }
+        
+      <div className="list row">
+        <div className="col-md-8">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by title"
+              value={searchTitle}
+              onChange={this.onChangeSearchTitle}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={this.searchTitle}
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-6">
+            <h4>Recipes List</h4>
+        
+            <ul className="list-group">
+              {userRecipes.recipes?.map((recipe, index) => (
+                <li 
+                  className={"list-group-item" +
+                  (index === currentIndex ? "active" : "")
+                }
+                onClick={() => this.setActiverecipe(recipe, index)}
+                  key={index}>
+                    {recipe.title}
+                </li>
+               ))}
+            </ul>
+        </div>
+   
+
+      <div className="col-md-6">
+          {currentrecipe ? (
+            <div>
+              <h4>Recipe</h4>
+              <div>
+                <label>
+                  <strong>Title:</strong>
+                </label>{" "}
+                {currentrecipe.title}
+              </div>
+              <div>
+                <label>
+                  <strong>Description:</strong>
+                </label>{" "}
+                {currentrecipe.description}
+              </div>
+              <div>
+                <label>
+                  <strong>Recipe Type:</strong>
+                </label>{" "}
+                {currentrecipe.recipeType}
+              </div>
+              <div>
+                <label>
+                  <strong>ServingSize:</strong>
+                </label>{" "}
+                {currentrecipe.servingSize}
+              </div>
+              <div>
+                <label>
+                  <strong>Ingredients:</strong>
+                </label>{" "}
+                {currentrecipe.ingredients}
+              </div>
+              <div>
+                <label>
+                  <strong>Directions:</strong>
+                </label>{" "}
+                {currentrecipe.directions}
+              </div>
+              <div>
+                <label>
+                  <strong>Contributed by:</strong>
+                </label>{" "}
+               ?
+              </div>
+
+              <Link
+                to={"/recipes/" + currentrecipe.id}
+                className="badge badge-warning"
+              >
+                View Full Recipe
+              </Link>
+              <Link
+                to={"/recipes/edit/" + currentrecipe.id}
+                className="badge badge-warning"
+              >
+                edit
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <br />
+              <p>Please click on a recipe...</p>
+            </div>
+          )}
+        </div>
+     </div>    
+   </>
+)}
 }
