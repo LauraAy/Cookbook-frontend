8da1b5d735e7d2fbcdab1f53e904237081c708fc
laura_ay <laura.ay1564@gmail.com>
2023-06-06 12:44:08 -0700

    updating recipe-list

--- src/components/recipes-list.component.js
+++ src/components/recipes-list.component.js
@@ -1,215 +1,185 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import recipeDataService from "../services/recipe.service";
 import { Link } from "react-router-dom";
 
-export default class RecipesList extends Component {
-  constructor(props) {
-    super(props);
-    this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
-    this.retrieverecipes = this.retrieverecipes.bind(this);
-    this.refreshList = this.refreshList.bind(this);
-    this.setActiverecipe = this.setActiverecipe.bind(this);
-    this.removeAllrecipes = this.removeAllrecipes.bind(this);
-    this.searchTitle = this.searchTitle.bind(this);
+const RecipesList = ()=> {
+  const [recipes, setRecipes] = useState ([]);
+  const [currentRecipe, setCurrentRecipe] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [searchTitle, setSearchTitle] = useState("");
 
-    this.state = {
-      recipes: [],
-      currentrecipe: null,
-      currentIndex: -1,
-      searchTitle: "",
-    };
-  }
+  useEffect(() => {
+  retrieveRecipes();
+}, []);
 
-  componentDidMount() {
-    this.retrieverecipes();
-  }
+const onChangeSearchTitle = e => {
+  const searchTitle = e.target.value;
+  setSearchTitle(searchTitle);
+};
 
-  onChangeSearchTitle(e) {
-    const searchTitle = e.target.value;
+const retrieveRecipes = () => {
+  recipeDataService.getAll()
+  .then(response => {
+    setRecipes(response.data);
+    console.log(response.data);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+};
 
-    this.setState({
-      searchTitle: searchTitle
-    });
-  }
 
-  retrieverecipes() {
-    recipeDataService.getAll()
-      .then(response => {
-        this.setState({
-          recipes: response.data
-        });
-        console.log(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
+const refreshList = () => {
+  retrieveRecipes();
+  setCurrentRecipe(null);
+  setCurrentIndex(-1);
+};
 
-  refreshList() {
-    this.retrieverecipes();
-    this.setState({
-      currentrecipe: null,
-      currentIndex: -1
-    });
-  }
+const setActiveRecipe = (recipe, index) => {
+  setCurrentRecipe(recipe);
+  setCurrentIndex(index);
+};
 
-  setActiverecipe(recipe, index) {
-    this.setState({
-      currentrecipe: recipe,
-      currentIndex: index
-    });
-  }
+const removeAllrecipes = () => {
+  recipeDataService.removeAll()
+  .then(response => {
+    console.log(response.data);
+    refreshList();
+  })
+  .catch(e => {
+    console.log(e);
+  });
+};
 
-  removeAllrecipes() {
-    recipeDataService.deleteAll()
-      .then(response => {
-        console.log(response.data);
-        this.refreshList();
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
+const findByTitle = () => {
+  recipeDataService.findByTitle(searchTitle)
+  .then (response => {
+    setRecipes(response.data);
+    console.log(response.data);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+};
 
-  searchTitle() {
-    this.setState({
-      currentrecipe: null,
-      currentIndex: -1
-    });
-
-    recipeDataService.findByTitle(this.state.searchTitle)
-      .then(response => {
-        this.setState({
-          recipes: response.data
-        });
-        console.log(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
-
-  render() {
-    const { searchTitle, recipes, currentrecipe, currentIndex } = this.state;
-
-    return (
-      <div className="list row">
-        <div className="col-md-8">
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search by title"
-              value={searchTitle}
-              onChange={this.onChangeSearchTitle}
-            />
-            <div className="input-group-append">
-              <button
-                className="btn btn-outline-secondary"
-                type="button"
-                onClick={this.searchTitle}
-              >
-                Search
-              </button>
-            </div>
+return (
+  <div className="list row">
+    <div className="col-md-8">
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search by title"
+          value={searchTitle}
+          onChange={onChangeSearchTitle}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              OnClick={findByTitle}
+            >
+              Search
+            </button>
           </div>
         </div>
-        <div className="col-md-6">
-          <h4>Recipes List</h4>
+      </div>
+      <div className="col-md-6">
+        <h4>Recipes List</h4>
 
-          <ul className="list-group">
-            {recipes &&
-              recipes.map((recipe, index) => (
-                <li
-                  className={
-                    "list-group-item " +
-                    (index === currentIndex ? "active" : "")
-                  }
-                  onClick={() => this.setActiverecipe(recipe, index)}
-                  key={index}
-                >
-                  {recipe.title}
+        <ul className="list-group">
+          {recipes &&
+          recipes.map((recipe, index) => (
+            <li
+              className={
+                "list-group-item " + (index === currentIndex ? "active" : "")
+                }
+              onClick={() => setActiveRecipe(recipe, index)}
+              key={index}
+            >
+              {recipe.title}
   
-                </li>
-              ))}
-          </ul>
+            </li>
+          ))}
+        </ul>
 
-          <button
-            className="m-3 btn btn-sm btn-danger"
-            onClick={this.removeAllrecipes}
-          >
-            Remove All
-          </button>
-        </div>
+        <button
+          className="m-3 btn btn-sm btn-danger"
+          onClick={removeAllrecipes}
+        >
+          Remove All
+        </button>
+      </div>
 
-        <div className="col-md-6">
-          {currentrecipe ? (
+      <div className="col-md-6">
+        {currentRecipe ? (
+          <div>
+            <h4>Recipe</h4>
             <div>
-              <h4>Recipe</h4>
-              <div>
-                <label>
-                  <strong>Title:</strong>
-                </label>{" "}
-                {currentrecipe.title}
+              <label>
+                <strong>Title:</strong>
+              </label>{" "}
+              {currentRecipe.title}
+            </div>
+            <div>
+              <label>
+                <strong>Description:</strong>
+              </label>{" "}
+              {currentRecipe.description}
+            </div>
+            <div>
+              <label>
+                <strong>Recipe Type:</strong>
+              </label>{" "}
+              {currentRecipe.recipeType}
               </div>
-              <div>
-                <label>
-                  <strong>Description:</strong>
-                </label>{" "}
-                {currentrecipe.description}
-              </div>
-              <div>
-                <label>
-                  <strong>Recipe Type:</strong>
-                </label>{" "}
-                {currentrecipe.recipeType}
-              </div>
-              <div>
-                <label>
-                  <strong>ServingSize:</strong>
-                </label>{" "}
-                {currentrecipe.servingSize}
-              </div>
-              <div>
-                <label>
-                  <strong>Ingredients:</strong>
-                </label>{" "}
-                {currentrecipe.ingredients}
-              </div>
-              <div>
-                <label>
-                  <strong>Directions:</strong>
-                </label>{" "}
-                {currentrecipe.directions}
-              </div>
-              <div>
-                <label>
-                  <strong>Contributed by:</strong>
-                </label>{" "}
+            <div>
+              <label>
+                <strong>ServingSize:</strong>
+              </label>{" "}
+              {currentRecipe.servingSize}
+            </div>
+            <div>
+              <label>
+                <strong>Ingredients:</strong>
+              </label>{" "}
+              {currentRecipe.ingredients}
+            </div>
+            <div>
+              <label>
+                <strong>Directions:</strong>
+              </label>{" "}
+              {currentRecipe.directions}
+            </div>
+            <div>
+              <label>
+                <strong>Contributed by:</strong>
+              </label>{" "}
                ?
-              </div>
+            </div>
 
-              <Link
-                to={"/recipes/" + currentrecipe.id}
-                className="badge badge-warning"
-              >
-                View Full Recipe
-              </Link>
-              <Link
-                to={"/recipes/edit/" + currentrecipe.id}
-                className="badge badge-warning"
-              >
-                edit
-              </Link>
-            </div>
+            <Link
+              to={"/recipes/" + currentRecipe.id}
+              className="badge badge-warning"
+            >
+              View Full Recipe
+            </Link>
+            <Link
+              to={"/recipes/edit/" + currentRecipe.id}
+              className="badge badge-warning"
+            >
+              Edit
+            </Link>
+          </div>
           ) : (
-            <div>
-              <br />
+          <div>
+            <br />
               <p>Please click on a recipe...</p>
-            </div>
+          </div>
           )}
         </div>
       </div>
     );
   }
-}
+
+  export default RecipesList;
