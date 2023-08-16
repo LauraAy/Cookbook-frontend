87575bdfbb58dcf21dc2dc5df312abce1acd3aa0
laura_ay <laura.ay1564@gmail.com>
2023-08-15 17:13:59 -0700

    pagination working

--- src/components/recipesAll.component.js
+++ src/components/recipesAll.component.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
 import recipeDataService from "../services/recipe.service";
 import { Link } from "react-router-dom";
-import { Autocomplete, TextField, Options} from '@mui/material';
+import { Autocomplete, TextField, Pagination} from '@mui/material';
+import { Box, List, Tag, ListItem, Divider } from "@chakra-ui/react";
+import usePagination from "../utils/pagination.util";
+
 
 const RecipesAll = ()=> {
+  //variables for set data and search status
   const [recipes, setRecipes] = useState ([]);
   const [currentRecipe, setCurrentRecipe] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(-1);
   const [selectedRecipe, setSelectedRecipe] = useState("");
   const [searchActive, setSearchActive] = useState(false);
 
+  
+//retrieve recipe function and useEffect to run retrieve recipes on load
   useEffect(() => {
   retrieveRecipes();
 }, []);
@@ -25,17 +31,29 @@ const retrieveRecipes = () => {
   });
 };
 
-const refreshList = () => {
-  retrieveRecipes();
-  setCurrentRecipe(null);
-  setCurrentIndex(-1);
-};
+//variables for set up pagination
+let [page, setPage] = useState(1);
+const PER_PAGE = 5;
+const count = Math.ceil(recipes.length / PER_PAGE);
+const _DATA = usePagination(recipes, PER_PAGE);
+// const refreshList = () => {
+//   retrieveRecipes();
+//   setCurrentRecipe(null);
+//   setCurrentIndex(-1);
+// };
 
+//
+const handleChange = (e, p) => {
+  setPage(p);
+  _DATA.jump(p);
+};
+//set recipe as active on click
 const setActiveRecipe = (recipe, index) => {
   setCurrentRecipe(recipe);
   setCurrentIndex(index);
 };
 
+//title search
 const findByTitle = () => {
   const searchTitle = selectedRecipe.title
   console.log(selectedRecipe.title)
@@ -51,11 +69,50 @@ const findByTitle = () => {
   });
 };
 
+//reset to initial state
 const resetAll = () => {
   retrieveRecipes()
   setSearchActive(false)
 }
 return (
+  <div>
+    <Box p="5">
+    <Pagination
+        count={count}
+        size="large"
+        page={page}
+        variant="outlined"
+        shape="rounded"
+        onChange={handleChange}
+      />
+
+      <List p="10" pt="3" spacing={2}>
+        {_DATA.currentData().map(v => {
+          return (
+            <ListItem key={v.title} listStyleType="disc">
+              <span>{v.title}: </span>{" "}
+              <Divider display="inline" orientation="vertical" />
+              <span> {v.description}</span>{" "}
+              <Divider display="inline" orientation="vertical" />
+              {/* <span>
+                <Tag color="#0f4211">${v.msrp}</Tag>
+              </span> */}
+            </ListItem>
+          );
+        })}
+      </List>
+
+      <Pagination
+        count={count}
+        size="large"
+        page={page}
+        variant="outlined"
+        shape="rounded"
+        onChange={handleChange}
+      />
+      </Box>
+
+
   <div className="list row">
     <div>
       {searchActive ? (
@@ -181,6 +238,7 @@ return (
           )}
         </div>
       </div>
+    </div>
     );
   }
 
