ab7644e6d7c916de6107b2f7545265127f5824fe
laura_ay <laura.ay1564@gmail.com>
2023-09-06 17:05:56 -0700

    regionRecipes all display working

--- src/components/regionRecipesAll.component.js
+++ src/components/regionRecipesAll.component.js
@@ -3,9 +3,10 @@ import RegionRecipeDataService from "../services/regionRecipe.service";
 import { Link } from "react-router-dom";
 import { Accordion, AccordionSummary, AccordionDetails, Autocomplete, Box, Button, Divider,  List, ListItem, ListItemButton,  
   ListItemIcon, ListItemText, Pagination, TextField,Typography, } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {ExpandMore, unfoldMore} from '@mui/icons-material';
 import usePagination from "../utils/pagination.util";
 import { useNavigate } from 'react-router-dom';
+import { FixedSizeList } from 'react-window';
 
 const RegionRecipesAll = ({clickTitle, clickCreator})=> {
   
@@ -53,8 +54,8 @@ const RegionRecipesAll = ({clickTitle, clickCreator})=> {
     _DATA.jump(p);
   };
 
-   //search functions for country and region
-   const findByCountry = () => {
+  //search functions for country and region
+  const findByCountry = () => {
     const searchCountry = selectedRegion.country
     console.log(selectedRegion.country)
 
@@ -62,7 +63,7 @@ const RegionRecipesAll = ({clickTitle, clickCreator})=> {
     .then (response => {
       setRegionRecipes(response.data);
       setSearchActive(true)
-      // setCountrySearch(true)
+      setCountrySearch(true)
       setCurrentRecipe(null)
       console.log(response.data);
     })
@@ -74,12 +75,13 @@ const RegionRecipesAll = ({clickTitle, clickCreator})=> {
   const findByRegion = () => {
     const searchRegionName = currentRegionName
     console.log(selectedRegion.regionName)
+    
     RegionRecipeDataService.findByRegionName(searchRegionName)
     .then (response => {
       setRegionRecipes(response.data);
       setSearchActive(true)
       setCurrentRecipe(null)
-      console.log(response.data);
+      console.log(response.data.recipe);
     })
     .catch(e => {
       console.log(e);
@@ -105,31 +107,33 @@ const RegionRecipesAll = ({clickTitle, clickCreator})=> {
   return (
     <div>
     {searchActive ? (
+      <>
+      {countrySearch ? (
       <Box>
-        <Typography variant="h5" gutterBottom>
-          Click to view full recipe.
-        </Typography>
         <Box p="10" pt="3" spacing={2}>
           {_DATA &&
             _DATA.currentData().map(regionRecipe => {
             return (
             <>
-              <Typography variant="h6">{regionRecipe.country}</Typography>
-              <Accordion>
-              <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                  >
-                    <Typography>Click to see Recipes</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-              
-             
+              <Typography variant="h5">Recipes from {regionRecipe.country}</Typography>
+              <Typography variant="subtitle1">
+                Scroll to see all recipes for this country. 
+                Click on a title to see full recipe.
+              </Typography>
+                <List
+                  sx={{
+                    width: '100%',
+                    maxWidth: 480,
+                    bgcolor: 'background.paper',
+                    position: 'relative',
+                    overflow: 'auto',
+                    maxHeight: 500, 
+                    '& ul': { padding: 0 }
+                  }}
+                >
                 {regionRecipe.recipe &&
                   regionRecipe.recipe.map((recipe, index) => (
                   <>
-                    <List>
                       <ListItemButton onClick={() => handleListItemClick(recipe)}>
                         <ListItem key={recipe.id} >
                           <ListItemText
@@ -139,15 +143,10 @@ const RegionRecipesAll = ({clickTitle, clickCreator})=> {
                         </ListItem>
                         <Divider />
                       </ListItemButton>
-                    </List>
-                    
                   </>
                   )
                 )}
-                    </AccordionDetails>
-                  </Accordion>
-                
-              
+                </List>
               </>
             );
           })}
@@ -156,6 +155,61 @@ const RegionRecipesAll = ({clickTitle, clickCreator})=> {
           <Button variant="outlined" onClick={resetAll}>Return to all recipes</Button>
         </Box>
       </Box>
+      ):(
+      <Box>
+        <Box p="10" pt="3" spacing={2}>
+        <Typography variant="h5">Recipes from {currentRegionName}</Typography>
+            <>
+              <Typography variant="subtitle1">
+                Scroll to see all recipes for this country. 
+                Click on a title to see full recipe.
+              </Typography>
+              <List
+                  sx={{
+                    width: '100%',
+                    maxWidth: 480,
+                    bgcolor: 'background.paper',
+                    position: 'relative',
+                    overflow: 'auto',
+                    maxHeight: 500, 
+                    '& ul': { padding: 0 }
+                  }}
+                >
+           {regionRecipes &&
+            regionRecipes.map(regionRecipe => {
+            return (
+            <>
+          
+                {regionRecipe.recipe &&
+                  regionRecipe.recipe.map((recipe, index) => (
+                  <>
+                      <ListItemButton onClick={() => handleListItemClick(recipe)}>
+                        <ListItem key={recipe.id} >
+                          <ListItemText
+                            primary={recipe.title}
+                            secondary={recipe.description}
+                          />
+                        </ListItem>
+                        <Divider />
+                      </ListItemButton>
+                  </>
+                  )
+                )}
+              
+              </>
+            );
+          })}
+            </List>
+              </>
+            {/* );
+          })} */}
+        </Box>
+        <Box m={4}>
+          <Button variant="outlined" onClick={resetAll}>Return to all recipes</Button>
+        </Box>
+        </Box>
+      )}
+      </>
       ):(
       <div>
         <Typography variant="h4" gutterBottom>
@@ -222,9 +276,22 @@ const RegionRecipesAll = ({clickTitle, clickCreator})=> {
                   return (
                   <>
                     <Typography variant="h6">{regionRecipe.country}</Typography>
-                    {regionRecipe.recipe &&
-                      regionRecipe.recipe.map((recipe, index) => (
-                        <List>
+                    <Typography variant="subtitle1">
+                      Scroll to see all recipes for this country. 
+                      Click on a title to see full recipe.
+                    </Typography>
+                    <List
+                      sx={{
+                        width: '100%',
+                        maxWidth: 480,
+                        bgcolor: 'background.paper',
+                        position: 'relative',
+                        overflow: 'auto',
+                        maxHeight: 300, 
+                        '& ul': { padding: 0 }
+                      }}>
+                        {regionRecipe.recipe &&
+                          regionRecipe.recipe.map((recipe, index) => (    
                           <ListItemButton onClick={() => handleListItemClick(regionRecipe)}>
                             <ListItem key={regionRecipe.id} >
                               <ListItemText
@@ -234,9 +301,9 @@ const RegionRecipesAll = ({clickTitle, clickCreator})=> {
                             </ListItem>
                             <Divider />
                           </ListItemButton>
-                        </List>
-                      )
-                    )}
+                        )
+                      )}
+                    </List>
                   </>
                 );
               })}
@@ -261,117 +328,5 @@ const RegionRecipesAll = ({clickTitle, clickCreator})=> {
     </div>
   )
 }
-    {/* <div className="col-md-6">
-			{countrySearch? (<h4>Recipes by Country</h4>)
-			:(
-			<div>
-				<h4>Recipes by Region</h4>
-				<h4>{currentRegionName}</h4>
-				<br></br>
-			</div>
-			)}
-  
-    <div>
-      {regionRecipes &&
-      regionRecipes.map((regionRecipe) => (
-      <div  key={regionRecipe.id}>
-        <div>
-          <h4>{regionRecipe.country}</h4>
-        </div>
-        <div>
-          <ul>
-            {regionRecipe.recipe &&
-            regionRecipe.recipe.map((recipe, index) => (
-              <li
-                className={
-                  "list-group-item" + (index === currentIndex ? "active" : "")
-                }
-                onClick={() => setActiveRecipe(recipe, index)}
-                key={index}
-              >
-                {recipe.title}
-              </li>
-            ))}	
-          </ul>
-        </div>
-      </div>
-      ))}
 
-      </div>
-    </div>
-
-    <div className="col-md-6">
-      {currentRecipe ? (
-        <div>
-          <h4>Recipe</h4>
-          <div>
-            <label>
-              <strong>Title:</strong>
-            </label>{" "}
-            {currentRecipe.title}
-          </div>
-          <div>
-            <label>
-              <strong>Description:</strong>
-            </label>{" "}
-            {currentRecipe.description}
-          </div>
-          <div>
-            <label>
-              <strong>Recipe Type:</strong>
-            </label>{" "}
-            {currentRecipe.recipeType}
-            </div>
-          <div>
-            <label>
-              <strong>ServingSize:</strong>
-            </label>{" "}
-            {currentRecipe.servingSize}
-          </div>
-          <div>
-            <label>
-              <strong>Ingredients:</strong>
-            </label>{" "}
-            {currentRecipe.ingredients}
-          </div>
-          <div>
-            <label>
-              <strong>Directions:</strong>
-            </label>{" "}
-            {currentRecipe.directions}
-          </div>
-          <div>
-            <label>
-              <strong>Contributed by:</strong>
-            </label>{" "}
-          ?
-          </div>
-
-          <Link
-            to={"/recipes/" + currentRecipe.id}
-          >
-            <button>
-            View Full Recipe
-            </button>
-          </Link>
-          <Link
-         
-            to={"/recipes/edit/" + currentRecipe.id}
-            className="badge badge-warning"
-          >
-            <button>
-            Edit
-            </button>
-          </Link>
-        </div>
-        ) : (
-        <div>
-          <br />
-            <p>Please click on a recipe...</p>
-        </div>
-        )}
-      </div>
-    </div> */}
-
-
-  export default RegionRecipesAll;
+export default RegionRecipesAll;
