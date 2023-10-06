ae452ca3b333993e2bc531c033ca73bfb10e11cf
laura_ay <laura.ay1564@gmail.com>
2023-10-05 17:16:25 -0700

    started working on splash image and text

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import {Box, Typography} from '@mui/material';
+import {Box, Card, CardMedia, Container, Typography} from '@mui/material';
 import UserService from "../services/user.service";
 import AuthService from "../services/auth.service";
+import cuttingBoard from "../images/cuttingBoard.png";
+import { red, purple, blue, green } from '@mui/material/colors';
 
 const HomeComponent = () => {
   const [currentUser, setCurrentUser] = useState(undefined);
@@ -17,15 +19,57 @@ const HomeComponent = () => {
 		}
 	}, []);
 
+  const styles = {
+    Typography: {
+      color: '#ffffff',
+      // outlineColor: red[500]
+   
+
+    },
+    media: {
+       height: 0,
+       paddingTop: '56.25%' // 16:9
+    },
+    card: {
+       position: 'relative',
+       textAlign: 'center',
+      
+    },
+    overlay: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      color: '#ffffff',
+      
+   
+      // color: 'black',
+      // backgroundColor: 'white'
+    }
+ }
+
   return (
   <>
     { currentUser ? (
     <>
-      <Box align='center' mt={2}>
-        <Typography variant='h2' color='primary.main'>
+
+{/* <Card style={styles.card}>
+   <CardMedia image={this.props.preview} style={styles.media}/>
+   <div style={styles.overlay}>
+      this text should overlay the image
+   </div>
+</Card> */}
+     
+    <Card style={styles.card}>
+        <CardMedia image={cuttingBoard} style={styles.media}/>
+        {/* <Box sx={{p: '30px', }}> */}
+        <Box>
+        <Typography variant='h2' color='primary.main' style={styles.overlay}>
           Welcome to Your World Cookbook
         </Typography>
-      </Box>
+        </Box>
+  
+        </Card>
     </>
     ):(
     <>
